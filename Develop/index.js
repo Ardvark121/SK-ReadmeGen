58b26d612d9ec425f58ce7ec144560b94cd7679e
Shawn Kim <kimshawnj@gmail.com>
2023-10-30 00:32:15 -0500

    finsihed linking the functions:

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,63 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs/promises");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is the name of your project",
+    name: "Name",
+  },
+  {
+    type: "input",
+    message: "Enter a short description of your project",
+    name: "Description",
+  },
+  {
+    type: "input",
+    message: "Usage",
+    name: "Usage",
+  },
+  {
+    type: "input",
+    message: "Contributing",
+    name: "Contributing",
+  },
+  {
+    type: "input",
+    message: "how did you test your code",
+    name: "Test",
+  },
+  {
+    type: "list",
+    name: "License",
+    message: "Choose a license",
+    choices: ["MIT", "Mozilla", "WTFPL", "Unlicense", "None"],
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+async function writeToFile(fs, data) {
+  const file = await generateMarkdown(data);
+  fs.writeFile("ReadMe.md", file, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+async function init() {
+  inquirer
+    .prompt(questions)
+    .then((answers) => {
+      writeToFile(answers);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,66 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if ((license = "MIT")) {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]";
+  }
+  if ((license = "Mozilla")) {
+    return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)]";
+  }
+  if ((license = "Unlicense")) {
+    return "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)]";
+  }
+  if ((license = "WTFPL")) {
+    return "[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)]";
+  }
+  if ((license = "None")) {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if ((license = "MIT")) {
+    return "https://opensource.org/licenses/ISC";
+  }
+  if ((license = "Mozilla")) {
+    return "https://opensource.org/licenses/MPL-2.0";
+  }
+  if ((license = "Unlicense")) {
+    return "http://unlicense.org/";
+  }
+  if ((license = "WTFPL")) {
+    return "http://www.wtfpl.net/about/";
+  }
+  if ((license = "None")) {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+async function renderLicenseSection(license) {
+  const badge = await renderLicenseBadge(license);
+  const link = await renderLicenseLink(license);
+  const section = badge + " " + link;
+  return section;
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+async function generateMarkdown(data) {
+  const licensesect = await renderLicenseSection(data.License);
+  const markdown = `# ${data.Name}
+  # Description
+  ${data.Description}
+  # Usage
+  ${data.Usage}
+  # Contact Us
+  ${data.Contributing}
+  # Test
+  ${data.Test}
+  ${licensesect}`;
+  return markdown;
 }
 
 module.exports = generateMarkdown;
